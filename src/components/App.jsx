b86fbc02a583a63f4678cd4859d69ea8ad7cd532
Андрій <rukom@ukr.net>
2024-01-29 00:56:56 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,14 @@
-// import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { FormAddContacts } from './FormAddContacts/FormAddContacts';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-// import { store } from '../redux/store';
+import { addContact, deleteContact, setFilter } from '../redux/reducer';
 
 export const App = () => {
-  // const [contacts, setContacts] = useState([]);
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   const savedContacts = localStorage.getItem('contacts');
-  //   const contactsList = JSON.parse(savedContacts);
-  //   if (contactsList && contactsList.length > 0) {
-  //     setContacts(contactsList);
-  //   }
-  // }, []);
-
   const dispatch = useDispatch();
   const contacts = useSelector(store => store.contactsGroup.contacts);
   const filter = useSelector(store => store.contactsGroup.filter);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
 
   const handleSubmitForm = formData => {
     const hasDuplicateName = contacts.some(
@@ -39,31 +22,18 @@ export const App = () => {
       id: nanoid(),
       ...formData,
     };
-    console.log(contactData);
-    // setContacts(prevContacts => [...prevContacts, contactData]);
-    const action = {
-      type: 'contacts/addContact',
-      payload: contactData,
-    };
+    const action = addContact(contactData);
     dispatch(action);
   };
 
   const handleDeleteContact = contactId => {
-    const action = {
-      type: 'contacts/deleteContact',
-      payload: contactId,
-    };
-
+    const action = deleteContact(contactId);
     dispatch(action);
   };
 
   const handleChangeFilter = event => {
     const value = event.target.value;
-    const action = {
-      type: 'contacts/setFilter',
-      payload: value,
-    };
-
+    const action = setFilter(value);
     dispatch(action);
   };
 
